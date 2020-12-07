27813b3c0173576ff006965ae8acc402f58df8c3
Vikram Chatterjee <strycora@gmail.com>
2020-12-07 14:47:43 -0700

    commit tree 65fc43245f6a42da37986353403daaec07a6a98e

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Friendly", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Black", "Hyper", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "White", "Relaxed", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "Brown", "Loud", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "Grey", "Jolly", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 6, "M", "Green", "Grumpy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 9, "F", "Black", "Calm", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 3, "M", "Black", "Angry", 0);
